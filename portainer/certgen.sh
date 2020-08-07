6ed52b310ebf6267a60fd424807a4273ea01a151
Cristian Asenjo <cristian.a.asenjo@gmail.com>
2020-08-06 22:31:19 -0400

    Refactor generation of certs

--- portainer/certgen.sh
+++ portainer/certgen.sh
@@ -2,23 +2,24 @@
 
 DOMAIN_NAME_EXTENSION=docker
 
+function generateCerts {
+    openssl genrsa -out $1.key 2048
+    openssl ecparam -genkey -name secp384r1 -out $1.key
+    openssl req -new -x509 -sha256 -key $1.key -out $1.crt -days 3650 -subj "/C=CA/ST=/L=/O=/OU=/CN=$1"
+}
+
 # Generate Portainer certs
 PORTAINER_CERT_FILE_NAME=portainer.$DOMAIN_NAME_EXTENSION
 if test -f "$PORTAINER_CERT_FILE_NAME.key"; then
     echo "Portainer certificates already exist"
 else
-    openssl genrsa -out $PORTAINER_CERT_FILE_NAME.key 2048
-    openssl ecparam -genkey -name secp384r1 -out $PORTAINER_CERT_FILE_NAME.key
-    openssl req -new -x509 -sha256 -key $PORTAINER_CERT_FILE_NAME.key -out $PORTAINER_CERT_FILE_NAME.crt -days 3650 -subj "/C=CA/ST=/L=/O=/OU=/CN=$PORTAINER_CERT_FILE_NAME"
+    generateCerts $PORTAINER_CERT_FILE_NAME
 fi
-
 
 # Generate Traefik certs
 TRAEFIK_CERT_FILE_NAME=traefik.$DOMAIN_NAME_EXTENSION
 if test -f "$TRAEFIK_CERT_FILE_NAME.key"; then
     echo "Traefik certificates already exist"
 else
-    openssl genrsa -out $TRAEFIK_CERT_FILE_NAME.key 2048
-    openssl ecparam -genkey -name secp384r1 -out $TRAEFIK_CERT_FILE_NAME.key
-    openssl req -new -x509 -sha256 -key $TRAEFIK_CERT_FILE_NAME.key -out $TRAEFIK_CERT_FILE_NAME.crt -days 3650 -subj "/C=CA/ST=/L=/O=/OU=/CN=$TRAEFIK_CERT_FILE_NAME"
+    generateCerts $TRAEFIK_CERT_FILE_NAME
 fi
