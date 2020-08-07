75d38da97dac6f458888f4ffe84f72c17d4021db
Cristian Asenjo <cristian.a.asenjo@gmail.com>
2020-08-06 22:20:23 -0400

    Add Traefik HTTPS cert generation with a little extra cleanup for flexibility

--- portainer/certgen.sh
+++ portainer/certgen.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-DOMAIN_NAME=portainer.docker
+DOMAIN_NAME_EXTENSION=docker
 
-if test -f "$DOMAIN_NAME.key"; then
+# Generate Portainer certs
+PORTAINER_CERT_FILE_NAME=portainer.$DOMAIN_NAME_EXTENSION
+if test -f "$PORTAINER_CERT_FILE_NAME.key"; then
     echo "Portainer certificates already exist"
-    exit 0
 fi
 
-openssl genrsa -out $DOMAIN_NAME.key 2048
-openssl ecparam -genkey -name secp384r1 -out $DOMAIN_NAME.key
-openssl req -new -x509 -sha256 -key $DOMAIN_NAME.key -out $DOMAIN_NAME.crt -days 3650 -subj "/C=CA/ST=/L=/O=/OU=/CN=$DOMAIN_NAME"
+openssl genrsa -out $PORTAINER_CERT_FILE_NAME.key 2048
+openssl ecparam -genkey -name secp384r1 -out $PORTAINER_CERT_FILE_NAME.key
+openssl req -new -x509 -sha256 -key $PORTAINER_CERT_FILE_NAME.key -out $PORTAINER_CERT_FILE_NAME.crt -days 3650 -subj "/C=CA/ST=/L=/O=/OU=/CN=$PORTAINER_CERT_FILE_NAME"
+
+# Generate Traefik certs
+TRAEFIK_CERT_FILE_NAME=traefik.$DOMAIN_NAME_EXTENSION
+if test -f "$TRAEFIK_CERT_FILE_NAME.key"; then
+    echo "Traefik certificates already exist"
+fi
+
+openssl genrsa -out $TRAEFIK_CERT_FILE_NAME.key 2048
+openssl ecparam -genkey -name secp384r1 -out $TRAEFIK_CERT_FILE_NAME.key
+openssl req -new -x509 -sha256 -key $TRAEFIK_CERT_FILE_NAME.key -out $TRAEFIK_CERT_FILE_NAME.crt -days 3650 -subj "/C=CA/ST=/L=/O=/OU=/CN=$TRAEFIK_CERT_FILE_NAME"
